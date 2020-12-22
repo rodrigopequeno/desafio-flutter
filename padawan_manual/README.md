35d10454f6031e436efbc95a770529be1c4e24c6
Rodrigo Pequeno <rodrigo.pequeno6@gmail.com>
2020-12-21 23:05:50 -0300

    Initial Commit

---
+++ padawan_manual/README.md
@@ -0,0 +1,3 @@
+# PADAWAN MANUAL
+
+The padawan manual shows the list of all the star war characters and the padawan can favorite your favorite characters.
---
+++ padawan_manual/android/app/src/main/kotlin/br/com/padawan_manual/padawan_manual/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.padawan_manual.padawan_manual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
